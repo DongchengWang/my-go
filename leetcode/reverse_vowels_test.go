54a8b872270882b5ec902268eee581df06c19287
Donald33 Nevermore <donald33wang@gmail.com>
2021-03-19 22:29:55 +0800

    Update tests

---
+++ leetcode/reverse_vowels_test.go
@@ -0,0 +1,11 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReverseVowels(t *testing.T) {
+	assert.Equal(t, 1, 1)
+}
--- leetcode/two_sum_test.go
+++ leetcode/two_sum_test.go
@@ -1,15 +1,14 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestTwoSum(t *testing.T) {
 	got := twoSum([]int{2, 7, 11, 15}, 9)
 	want := []int{1, 2}
 
-	for i, v := range got {
-		if v != want[i] {
-			t.Errorf("got %q, wanted %q", got, want)
-		}
-	}
-
+	assert.Equal(t, got, want)
 }
