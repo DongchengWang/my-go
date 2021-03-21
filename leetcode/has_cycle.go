e23a4f9b519cb6592e901096b8da673871ab0fc7
Donald33 Nevermore <donald33wang@gmail.com>
2021-03-21 23:26:50 +0800

    Add leetcode 141

---
+++ leetcode/has_cycle.go
@@ -0,0 +1,26 @@
+package leetcode
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+
+	l1 := head
+	l2 := head.Next
+
+	for l1 != nil && l2 != nil && l2.Next != nil {
+		if l1 == l2 {
+			return true
+		}
+		l1 = l1.Next
+		l2 = l2.Next.Next
+	}
+
+	return false
+}
