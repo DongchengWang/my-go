bdbd6cb67af2e399dee8f1046b3ee8bbffac6a39
Donald33 Nevermore <donald33wang@gmail.com>
2021-03-22 09:43:02 +0800

    Update has_cycle

--- leetcode/has_cycle.go
+++ leetcode/has_cycle.go
@@ -14,12 +14,16 @@ func hasCycle(head *ListNode) bool {
 	l1 := head
 	l2 := head.Next
 
-	for l1 != nil && l2 != nil && l2.Next != nil {
+	for l1 != nil && l2 != nil {
 		if l1 == l2 {
 			return true
 		}
+
 		l1 = l1.Next
-		l2 = l2.Next.Next
+		l2 = l2.Next
+		if l2 != nil {
+			l2 = l2.Next
+		}
 	}
 
 	return false
