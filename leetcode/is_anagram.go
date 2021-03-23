7119afc6758684436e9d8575c08d13b46f35fba1
Donald33 Nevermore <donald33wang@gmail.com>
2021-03-23 23:26:21 +0800

    Add leetcode 242

---
+++ leetcode/is_anagram.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func isAnagram(s string, t string) bool {
+	count := [26]int{}
+	for _, val := range s {
+		count[val-'a']++
+	}
+	for _, val := range t {
+		count[val-'a']--
+	}
+
+	for _, val := range count {
+		if val != 0 {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ leetcode/is_anagram_test.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsAnagram(t *testing.T) {
+	s1 := "anagram"
+	s2 := "nagaram"
+
+	got := isAnagram(s1, s2)
+	assert.True(t, got)
+}
+
+func TestIsAnagram2(t *testing.T) {
+	s1 := "rat"
+	s2 := "car"
+
+	got := isAnagram(s1, s2)
+	assert.False(t, got)
+}
